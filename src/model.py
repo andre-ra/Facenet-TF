d46f76fffdd5cba78654a53a8c8bfd934254090c
andre-ra <andre.riesco97@gmail.com>
2022-07-18 20:52:02 -0300

    modificacoes feitas em model.py
    Signed-off-by: Andre Riesco <andre.riesco97@gmail.com>

--- src/model.py
+++ src/model.py
@@ -9,8 +9,8 @@ class face_model(tf.keras.Model):
     
     def __init__(self, params):
         super(face_model, self).__init__()
-        img_size = (params.image_size, params.image_size, 3)
-        self.base_model = tf.keras.applications.InceptionV3(include_top=False, input_shape=img_size)
+        img_size = (params.image_height, params.image_width, 3)
+        self.base_model = tf.keras.applications.InceptionV3(include_top=False, input_shape=img_size, weights='imagenet')
         self.base_model.trainable = False
         self.flatten = tf.keras.layers.Flatten()
         self.embedding_layer = tf.keras.layers.Dense(units=params.embedding_size)
